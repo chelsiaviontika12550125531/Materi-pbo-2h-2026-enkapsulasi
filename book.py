9639ceaab4c25a3466aee5b02caf4fef04354fce
chelsia viontika manalu <12550125531@students.uin-suska.ac.id>
2026-03-13 15:17:31 +0700

    little revisi for python

--- book.py
+++ book.py
@@ -6,3 +6,16 @@ class Book:
         self.title = title
         self.author = author
         self.last_updated = datetime.now()
+    
+    def get_id(self):
+        return self.id
+    
+    def get_title(self):
+        return self.title
+    def set_title(self, new_title):
+        self.title = new_title
+    
+    def get_author(self):
+        return self.author
+    def set_author(self, new_author):
+        self.author = new_author
