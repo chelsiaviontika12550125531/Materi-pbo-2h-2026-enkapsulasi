7c9d483f931fff90dcc47d412689bfd8b5014ce3
chelsia viontika manalu <12550125531@students.uin-suska.ac.id>
2026-03-13 15:18:19 +0700

    little revisi for output
    Updated book IDs, titles, and authors. Added methods to get and set book details.

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
 from book import Book
 
-b1 = Book(1, "Learn Python", "Affan")
-b2 = Book(2, "Mastering OOP", "Budi")
+b1 = Book(2407, "Learn Python ", "Affan")
+b2 = Book(5783, "Mastering OOP ", "Budi")
 
-print(b1.title)
-print(b2.title)
+print(b1.get_title() + b1.get_author())
+print(b2.get_title() + b2.get_author())
+
+b1.set_title("belajar python with doctor ")
+b2.set_title("mastering OOP with mr ")
+
+b1.set_author("affandes")
+b2.set_author("budiman")
+
+print(b1.get_title()+ b1.get_author())
+print(b2.get_title()+ b2.get_author())
